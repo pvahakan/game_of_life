baebec2592cebe5e2c4e5acf6f4953c32f7ef8c6
pvahakan <pauli.vahakangas@gmail.com>
2018-11-17 23:25:11 +0200

    Main file fixed to use classes

--- game.py
+++ game.py
@@ -5,112 +5,34 @@ import numpy as np
 import os
 import time
 from life import Life
-
-# display_width = 800
-# display_height = 600
-# 
-# cell_size = 20 # Color block size
-# 
-# # Calculating number of color boxes
-# x = np.linspace(0, display_width, display_width/cell_size)
-# y = np.linspace(0, display_height, display_height/cell_size)
-# 
-# # Columns and rows opposite to the (x,y) notation
-# board = np.zeros((len(y), len(x)))
-# 
-# # Colors
-# green = (0, 255, 0)
-# white = (255, 255, 255)
-
-def graphix():
-    # Initialize
-    pygame.init()
-
-    # Create window
-    gameDisplay = pygame.display.set_mode((display_width, display_height))
-    pygame.display.set_caption("Conway's game of life")
-
-    clock = pygame.time.Clock()
-
-    crashed = False
-
-    # Game loop
-    # while not crashed:
-    gameDisplay.fill(white)
-
-    for xi in x:
-        pygame.draw.rect(gameDisplay, green, [xi, xi, cell_size, cell_size])
-        
-    pygame.display.update()
-    clock.tick(40) # FPS
-
-    pygame.quit()
-    quit()
-
-def init_window(width, height):
-    """
-    Initializes the PyGame window
-    :return display:    PyGame display object.
-    :return clock:      PyGame clock object.
-    """
-    pygame.init()
-    display = pygame.display.set_mode((width, height))
-    pygame.display.set_caption("Graphics window")
-    clock = pygame.time.Clock()
-    display.fill(white)
-
-    return display, clock
-
-
-
-def draw_array(display, array, cell_size):
-    """
-    Function to draw occupied cells in array.
-    :param display:     PyGame display, figure will be drawn in this.
-    :param array:       Numpy array, array of cells. 
-                        Cell value of 1 means occupied and 0 unoccupied
-    :param cell_size:   tuple, the size of cell to be drawn.
-    """
-    x, y = 0, 0
-
-    # Find occupied cells
-    for i in range(array_width):
-        x = 0
-        for j in range(array_height):
-            if array[i][j] == 1:
-                pygame.draw.rect(
-                        display, green, [x, y, cell_size[0], cell_size[1]]
-                        )
-            x += cell_size[0]
-        y += cell_size[1]
-
-    pygame.display.update()
+from graphix import Graphix
 
 if __name__ == "__main__":
-    # g = Life(20, 20)
-    # g.glider_init()
-    # g.small_exploder_init()
 
     # Window parameters
     window_width = 600
     window_height = 600
 
-    # Color definitions
-    white = (255, 255, 255)
-    green = (0, 255, 0)
+    # Create objects
+    game = Life(60, 60)
+    gpx = Graphix(window_width, window_height)
 
-    # Create array to be plotted
-    array_width, array_height = 60, 60
-    array = np.random.randint(2, size=(array_width, array_height))
+    game.random_init()
 
-    # Calculating cell size
-    cell_x = window_width / len(array[0])
-    cell_y = window_height / len(array)
+    # Calculating cell size to draw
+    cell_x = gpx.window_width / len(game.board[0])
+    cell_y = gpx.window_height / len(game.board)
 
-    # Init and show
-    display, clock = init_window(window_width, window_height)
-    draw_array(display, array, (cell_x, cell_y))
+    # Game loop
+    i = 0
+    while i < 100:
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT:
+            break
+        gpx.draw_array(gpx.display, game.board, (cell_x, cell_y))
+        game.logic()
+        gpx.clock.tick(20)
+        gpx.display.fill(gpx.white)
+        i += 1
 
-    print(array)
-    time.sleep(9)
     pygame.quit()
